38fd2f0376f28a7f043e2054c7170df3c5c41d49
vedant8767 <155797741+vedant8767@users.noreply.github.com>
2026-03-08 21:45:22 +0530

    error page added

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,20 +1,31 @@
- export const metadata = {
-  title:"Profile"
- }
+export const metadata = {
+  title: "Profile",
+};
 
-function generateRandomNumber(count){
-  return Math.floor(Math.random()*count)
+function generateRandomNumber(count) {
+  return Math.floor(Math.random() * count);
 }
 
 function page() {
-  const random = generateRandomNumber(2)
-  if(random === 1){
-    throw new Error("Something went wrong")
+  const random = generateRandomNumber(2);
+  if (random === 1) {
+    throw new Error("Something went wrong");
   }
 
   return (
-    <div className="h-56 bg-blue-500 text-center font-bold text-2xl text-white">Profile</div>
-  )
+    <div className="h-56 bg-blue-500 text-center font-bold text-2xl text-white flex flex-col items-center justify-center gap-4">
+      <div>Profile</div>
+
+      <button
+        onClick={() => {
+          throw new Error("Something went wrong");
+        }}
+        className="bg-white text-black px-4 py-2 rounded"
+      >
+        Generate Error
+      </button>
+    </div>
+  );
 }
 
-export default page
+export default page;
